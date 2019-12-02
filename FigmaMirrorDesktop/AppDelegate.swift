a1ecf1f874ca344efae4f282e03febdca20eab43
Nate Parrott <nateparro2t@gmail.com>
2019-12-01 23:32:56 -0500

    Initial Commit

---
+++ FigmaMirrorDesktop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FigmaMirrorDesktop
+//
+//  Created by Nate Parrott on 12/1/19.
+//  Copyright © 2019 Nate Parrott. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
